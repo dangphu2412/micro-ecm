0ffe4bad05000574365d6315b4aba96e56e53715
jackdang <dangphu241299@gmail.com>
2022-04-09 12:18:08 +0700

    Setup: Setup core & common for packages

---
+++ packages/core/src/bootstrap.ts
@@ -0,0 +1,15 @@
+export class EcmBootstrap {
+    // constructor(injector) {
+    //     this.injector = injector;
+    //     this.bootstrap();
+    // }
+    // bootstrap() {
+    //     const bootstrapComponents = [
+    //         EcmMainComponent
+    //     ];
+    //     const appRef = this.injector.get(ApplicationRef);
+    //     const componentFactoryResolver = this.injector.get(ComponentFactoryResolver);
+    //     const componentFactory = componentFactoryResolver.resolveComponentFactory(EcmMainComponent);
+    //     const componentRef = appRef.bootstrap(componentFactory);
+    // }
+}
