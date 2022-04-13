ab4cccad5f303fddf21e19db818dbcdd9cac6feb
jackdang <dangphu241299@gmail.com>
2022-04-13 22:42:06 +0700

    chore: test publish core and microservices

--- packages/core/src/constant/metadata.constant.ts
+++ packages/core/src/constant/metadata.constant.ts
@@ -1,7 +1,7 @@
 export const CoreMetadata = {
   AUTO_CONFIG: 'core:auto-config',
-  // Chilli is a definition for singleton instance in our application
-  CONTROLLER: 'chilli:controller',
-  PROVIDER: 'chilli:provider',
-  REPOSITORY: 'chilli:repository',
+
+  CONTROLLER: 'ecm:controller',
+  PROVIDER: 'ecm:provider',
+  REPOSITORY: 'ecm:repository',
 }
